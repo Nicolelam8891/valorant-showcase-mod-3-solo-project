bfb432358c8008fc8e4065192976476abfb45fcc
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-07 11:34:41 -0700

    Set up link for character cards successfully

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
 import CharacterContainer from "../CharacterContainer/CharacterContainer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CharacterDetails from "../CharacterDetails/CharacterDetails";
 
 const App = () => {
   return (
     <Router>
-      
       <main className='App'>
         <h1>Valorant</h1>
         <CharacterContainer />
       </main>
+
+      <Routes>
+        <Route
+          path='/characterDetails/:id'
+          element={<CharacterDetails />}
+        ></Route>
+      </Routes>
     </Router>
   );
 };
