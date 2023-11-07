fcf210815bd8fce7ce822369372effaad501f465
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-07 11:21:04 -0700

    Set up Router successfuly

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,17 @@
-import './App.css';
-import CharacterContainer from '../CharacterContainer/CharacterContainer';
+import "./App.css";
+import CharacterContainer from "../CharacterContainer/CharacterContainer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
-
-
-
   return (
-    <main className="App">
-      <h1>Valorant</h1>
-      <CharacterContainer />
-    </main>
+    <Router>
+      
+      <main className='App'>
+        <h1>Valorant</h1>
+        <CharacterContainer />
+      </main>
+    </Router>
   );
-}
+};
 
 export default App;
