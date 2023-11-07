0b9bad458e864e829c4090cc3e03d27d3bfd3dac
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-07 13:31:47 -0700

    Get individual character details to console.log in CharacterDetail.js

--- src/components/CharacterDetails/CharacterDetails.js
+++ src/components/CharacterDetails/CharacterDetails.js
@@ -1,7 +1,39 @@
-import './CharacterDetails.css'
+import { useEffect, useState } from "react";
+import "./CharacterDetails.css";
+import { useParams } from "react-router-dom";
+
 
 const CharacterDetails = () => {
+  const [character, setCharacter] = useState([]);
+  const [error, setError] = useState([]);
+  const { id } = useParams();
 
-}
+  const getCharacterDetail = () => {
+    fetch(`https://valorant-api.com/v1/agents/${id}`)
+      .then(response => {
+        if(!response.ok) {
+          throw new Error(`Sorry, there is an error! status: ${response.status}. Please try again later`)
+        }
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        setCharacter([...character, data])
+      })
+      .catch(event => {
+        console.error(event.message)
+        setError(event.message)
+      })
+  };
 
-export default CharacterDetails
+  useEffect(() => {
+    getCharacterDetail();
+  }, []);
+};
+
+return (
+  <main className="character-details">
+    
+  </main>
+)
+export default CharacterDetails;
