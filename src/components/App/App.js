23ea76204625a538abf7b82f96993e969190c440
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-08 14:09:50 -0700

    Successful error message when user attempts to add more than 5 players

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,22 +4,34 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CharacterDetails from "../CharacterDetails/CharacterDetails";
 import Header from "../Header/Header";
 import TeamOne from "../TeamOne/TeamOne";
-import { useState } from 'react'
-
+import { useState } from "react";
 
 const App = () => {
+  const [teamOneCharacters, setTeamOneCharacters] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const [teamOneCharacters, setTeamOneCharacters] = useState([])
+  const addToTeam = (characterObject) => {
+    setErrorMessage(""); //this will reset the error message each time a user tries to add a character.
 
-  const addToTeam = ( characterObject ) => {
-    if (!teamOneCharacters.some(teamOneCharacter => teamOneCharacter.uuid === characterObject.uuid)) {
-      setTeamOneCharacters([...teamOneCharacters, characterObject])
+    //conditional for team page
+    if (teamOneCharacters.length >= 5) {
+      setErrorMessage("You can only add a maximum of 5 players to a team.");
+    } else if (
+      !teamOneCharacters.some(
+        (teamOneCharacter) => teamOneCharacter.uuid === characterObject.uuid
+      )
+    ) {
+      setTeamOneCharacters((prevTeamOneCharacters) => [
+        ...teamOneCharacters,
+        characterObject,
+      ]);
     }
-  }
+  };
 
   return (
     <Router>
       <main className='App'></main>
+      {errorMessage && <div className='team-error-message'>{errorMessage}</div>}
       <Routes>
         <Route
           path='/'
@@ -37,14 +49,14 @@ const App = () => {
               <Header showHomeButton={true} showTeamButton={true} />
               <CharacterDetails addToTeam={addToTeam} />
             </>
-          } 
+          }
         />
         <Route
           path='/team'
           element={
             <>
               <Header showHomeButton={true} showTeamButton={false} />
-              <TeamOne teamOneCharacters={teamOneCharacters}/>
+              <TeamOne teamOneCharacters={teamOneCharacters} />
             </>
           }
         />
