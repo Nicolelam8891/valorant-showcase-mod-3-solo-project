2b7c295f8aad6428cde640faeb2e323029b436b6
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-08 16:34:21 -0700

    Create team character cards and add CSS styling to display page

--- src/components/TeamOne/TeamOne.js
+++ src/components/TeamOne/TeamOne.js
@@ -1,26 +1,28 @@
 import "./TeamOne.css";
 
-const TeamOne = ( { teamOneCharacters } ) => {
+const TeamOne = ({ teamOneCharacters }) => {
   console.log("teamOneCharacters:=====", teamOneCharacters);
-  
 
   return (
-    <div className="team-one-container">
+    <div className="team-one-page">
       <div className='line'></div>
-      <div className="display-icon-image-container">
+      <div className='team-one-container'>
+        <div className='display-icon-image-card'>
           {teamOneCharacters.map((teamOneCharacter) => (
-          <div key={teamOneCharacter.uuid}> 
-            <img 
-              className='display-icon-image'
-              src={teamOneCharacter.displayIconSmall}
-              alt={teamOneCharacter.displayName}
-            />
+            <div className='team-icon-info' key={teamOneCharacter.uuid}>
+              <img
+                className='display-icon-image'
+                src={teamOneCharacter.displayIconSmall}
+                alt={teamOneCharacter.displayName}
+              />
+              <p className='character-name'>{teamOneCharacter.displayName}</p>
+              <button className="delete-button">❌</button> 
             </div>
           ))}
+        </div>
       </div>
     </div>
   );
 };
 
-export default TeamOne
-
+export default TeamOne;
