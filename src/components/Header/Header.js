7c016526e0d1b02ae1d9ed21c6bb6f59ce86a922
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-07 18:49:49 -0700

    Home button CSS for single character page completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,21 @@
-import './Header.css';
-import { Link } from 'react-router-dom';
-
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <header> 
-      <Link to='/'>
-          <img src="valorant.png" className='valorant-logo'/>
-      </Link>
-      <Link to='/'>
-        <button className='home-button'> BUTTON </button>
-      </Link>
+      <header>
+        <Link to='/'>
+          <img src='valorant.png' className='valorant-logo' />
+        </Link>
+        
+          <Link to='/' className="no-underline">
+            <button className='home-button'> HOME </button>
+          </Link>
+       
       </header>
-  
     </div>
-    )
-}
+  );
+};
 
 export default Header;
