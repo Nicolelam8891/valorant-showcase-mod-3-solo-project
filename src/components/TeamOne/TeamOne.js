c8652ee24fa219defa8eebd18643f3282aea322f
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-07 22:12:57 -0700

    Add a red line for all pages under header

---
+++ src/components/TeamOne/TeamOne.js
@@ -0,0 +1,11 @@
+import "./TeamOne.css";
+
+const TeamOne = () => {
+  return (
+    <div className="team-one-container">
+      <div className='line'></div>
+    </div>
+  );
+};
+
+export default TeamOne
