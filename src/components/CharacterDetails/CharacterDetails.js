1d0f39c28124c1950d4e2b28d77a7c1dbd8b804d
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-08 23:59:46 -0700

    Add CSS styling to single character page and style all buttons

--- src/components/CharacterDetails/CharacterDetails.js
+++ src/components/CharacterDetails/CharacterDetails.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./CharacterDetails.css";
 import { useParams } from "react-router-dom";
 
-const CharacterDetails = ( { addToTeam, errorMessage, confirmationMessage}) => {
+const CharacterDetails = ({ addToTeam, errorMessage, confirmationMessage }) => {
   const [character, setCharacter] = useState(null);
   const [error, setError] = useState("");
   const { id } = useParams();
@@ -38,49 +38,60 @@ const CharacterDetails = ( { addToTeam, errorMessage, confirmationMessage}) => {
   return (
     <div className='single-character-page'>
       <div className='character-details'>
-      {errorMessage && <div className='team-error-message'>{errorMessage}</div>}
-      {confirmationMessage && <div className='team-confirmation-message'>{confirmationMessage}</div>}
 
-      <div className="character-full-portrait-container">
-      <button className="add-to-team-button" onClick={() => addToTeam(character)}>Add to Team</button>
-        <img src={character.fullPortrait} className='character-full-portrait' />
-      </div>
-
-      <div className="character-info-container">
-        <p className='single-character-name'>
-          <strong>Name: </strong>
-          {character.displayName}
-        </p>
-        <p className='single-character-description'>
-          <strong>Description: </strong>
-          {character.description}
-        </p>
-        <p className='single-character-role'>
-          <strong>Role: </strong>
-          {character.role.displayName}
-        </p>
-        <p className='single-character-role-description'>
-          <strong>Role description: </strong> {character.role.description}
-        </p>
-        <h3 className="abilities-name">
-          <strong>Abilities: </strong>
-        </h3>
-      
-        <div className='ability-section'>
-          <ul className='ability-details'>
-            {character.abilities.map((ability) => (
-              <div className='ability-slots'>
-                <img
-                  class='ability-icon'
-                  src={ability.displayIcon}
-                  alt={ability.displayName}
-                />
-                <h3 className='ability-name'>{ability.displayName}</h3>
-                <p className='ability-description'>{ability.description}</p>
-              </div>
-            ))}
-          </ul>
+        <div className='character-full-portrait-container'>
+          <button
+            className='add-to-team-button'
+            onClick={() => addToTeam(character)}
+            >
+            Add to Team
+          </button>
+            {errorMessage && (
+              <div className='team-error-message'>{errorMessage}</div>
+            )}
+            {confirmationMessage && (
+              <div className='team-confirmation-message'>{confirmationMessage}</div>
+            )}
+          <img
+            src={character.fullPortrait}
+            className='character-full-portrait'
+          />
         </div>
+
+        <div className='character-info-container'>
+          <p className='single-character-name'>
+            <strong>Name: </strong>
+            {character.displayName}
+          </p>
+          <p className='single-character-description'>
+            {character.description}
+          </p>
+          <p className='single-character-role'>
+            <strong>Role: </strong>
+            {character.role.displayName}
+          </p>
+          <p className='single-character-role-description'>
+            {character.role.description}
+          </p>
+          <h3 className='abilities-name'>
+            <strong>Abilities: </strong>
+          </h3>
+
+          <div className='ability-section'>
+            <ul className='ability-details'>
+              {character.abilities.map((ability) => (
+                <div className='ability-slots'>
+                  <img
+                    class='ability-icon'
+                    src={ability.displayIcon}
+                    alt={ability.displayName}
+                  />
+                  <h3 className='ability-name'>{ability.displayName}</h3>
+                  <p className='ability-description'>{ability.description}</p>
+                </div>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
       {/* {error && <p className='error-message'>{error}</p>} */}
