f3e14e9ffd707bc8eccb7ebdd3d2857333668a97
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-10 12:11:19 -0700

    Able to get the first part of cypress working for the individualPage

--- cypress/e2e/individualPage.cy.js
+++ cypress/e2e/individualPage.cy.js
@@ -5,42 +5,55 @@ describe("Show selected character user flow", () => {
       "https://valorant-api.com/v1/agents?isPlayableCharacter=true",
       {
         statusCode: 200,
-        fixture: "gekkoCard",
+        fixture: "characterCards",
       }
     ).as("homepage");
-    cy.visit("http://localhost:3000/");
+    cy.intercept(
+      "GET",
+      "https://valorant-api.com/v1/agents/e370fa57-4757-3604-3648-499e1f642d3f",
+      {
+        statusCode: 200,
+        fixture: "gekkoCard",
+      }
+    ).as("gekko");
+    cy.visit(
+      "http://localhost:3000/characterDetails/e370fa57-4757-3604-3648-499e1f642d3f"
+    );
   });
 
-  it("should display header, drop-down, buttons and 24 character cards on load", () => {
-    cy.wait("@homepage").then((interception) => {
+  it("should display individual character details", () => {
+    cy.wait("@gekko").then((interception) => {
       cy.get("header")
         .get(".valorant-logo")
-        .get('.line')
+        .get(".line")
         .get(".home-button")
         .contains("HOME")
-        .get(".character-container")
-        .get(".card")
-        .should("have.length", 24)
-        .get(
-          '[href="/characterDetails/e370fa57-4757-3604-3648-499e1f642d3f"] > .card'
+        .get(".character-info-container")
+        .get(".single-character-name")
+        .contains("p", "Name: Gekko")
+        .get(".single-character-description")
+        .contains(
+          "p",
+          "Gekko the Angeleno leads a tight-knit crew of calamitous creatures. His buddies bound forward, scattering enemies out of the way, with Gekko chasing them down to regroup and go again."
         )
-        .first()
-        .get(".role-name-container")
-        .get(".character-name")
-        .contains("p", "Gekko")
-        .get(".character-role")
+        .get(".single-character-role")
         .contains("p", "Role: Initiator")
-        .get(
-          '[href="/characterDetails/e370fa57-4757-3604-3648-499e1f642d3f"] > .card'
+        .get(".single-character-role-description")
+        .contains(
+          "p",
+          "Initiators challenge angles by setting up their team to enter contested ground and push defenders away."
         )
-        .first()
+        .get(".abilities-name > strong")
+        .contains("Abilities:")
+        .get(".ability-details")
+        .get(".ability-details > :nth-child(1)")
+        .get(":nth-child(1) > .ability-icon")
         .find("img")
         .should(
           "have.attr",
           "src",
-          "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/displayicon.png"
-        ).click()
-      });
+          "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/abilities/ability1/displayicon.png"
+        );
     });
   });
-  
+});
